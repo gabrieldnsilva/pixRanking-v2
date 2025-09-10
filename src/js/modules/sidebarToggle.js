ad393616c8d5f2b6864a356206560b513c01b658
gabrildnsilva <gabrieldnsilva@gmail.com>
2025-09-10 13:14:40 -0300

    feat: dynamic sidebar styles, module sidebarToggle.js with JQuery integration

---
+++ src/js/modules/sidebarToggle.js
@@ -0,0 +1,29 @@
+/**
+ * Lógica para controlar a sidebar dinâmica
+ * Adiciona um evento de clique ao botão de menu que alterna a classe
+ * 'sidebar-collapsed' no elemento body, controlando a visibilidade da sidebar.
+ * @author Gabriel D. N. Silva
+ */
+
+const $sidebarToggle = $("#sidebar-toggle");
+const $body = $("body");
+
+if ($sidebarToggle.length) {
+	$sidebarToggle.on("click", function () {
+		$body.toggleClass("sidebar-collapsed");
+
+		const $icon = $(this).find("i");
+
+		if ($body.hasClass("sidebar-collapsed")) {
+			$icon
+				.removeClass("ri-arrow-left-s-line")
+				.addClass("ri-arrow-right-s-line");
+			$(this).attr("title", "Expandir Menu");
+		} else {
+			$icon
+				.removeClass("ri-arrow-right-s-line")
+				.addClass("ri-arrow-left-s-line");
+			$(this).attr("title", "Recolher Menu");
+		}
+	});
+}
