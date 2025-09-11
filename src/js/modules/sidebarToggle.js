7ffa66ad1f86ea3378b8681d2e31694a2319c3af
gabrildnsilva <gabrieldnsilva@gmail.com>
2025-09-10 23:53:39 -0300

    fix: .ready(function())) to sidebar js works properly

--- src/js/modules/sidebarToggle.js
+++ src/js/modules/sidebarToggle.js
@@ -2,28 +2,31 @@
  * Lógica para controlar a sidebar dinâmica
  * Adiciona um evento de clique ao botão de menu que alterna a classe
  * 'sidebar-collapsed' no elemento body, controlando a visibilidade da sidebar.
- * @author Gabriel D. N. Silva
  */
+export function initSidebarToggle() {
+	const $sidebarToggle = $("#sidebar-toggle");
+	const $body = $("body");
 
-const $sidebarToggle = $("#sidebar-toggle");
-const $body = $("body");
+	if ($sidebarToggle.length) {
+		$sidebarToggle.on("click", function () {
+			$body.toggleClass("sidebar-collapsed");
 
-if ($sidebarToggle.length) {
-	$sidebarToggle.on("click", function () {
-		$body.toggleClass("sidebar-collapsed");
+			const $icon = $(this).find("i");
 
-		const $icon = $(this).find("i");
-
-		if ($body.hasClass("sidebar-collapsed")) {
-			$icon
-				.removeClass("ri-arrow-left-s-line")
-				.addClass("ri-arrow-right-s-line");
-			$(this).attr("title", "Expandir Menu");
-		} else {
-			$icon
-				.removeClass("ri-arrow-right-s-line")
-				.addClass("ri-arrow-left-s-line");
-			$(this).attr("title", "Recolher Menu");
-		}
-	});
+			if ($body.hasClass("sidebar-collapsed")) {
+				$icon
+					.removeClass("ri-arrow-left-s-line")
+					.addClass("ri-arrow-right-s-line");
+				$(this).attr("title", "Expandir Menu");
+			} else {
+				$icon
+					.removeClass("ri-arrow-right-s-line")
+					.addClass("ri-arrow-left-s-line");
+				$(this).attr("title", "Recolher Menu");
+			}
+		});
+	}
 }
+
+// Initialize the sidebar toggle when the document is ready
+$(document).ready(function () {});
