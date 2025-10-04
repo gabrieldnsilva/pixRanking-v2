3b6c3d31ddcc21da9ef2c245b5e5884765ee9037
gabrildnsilva <gabrieldnsilva@gmail.com>
2025-10-04 11:39:29 -0300

    sql: Built ranking_db.sql reference of db-ClassDiagram

---
+++ api/database/ranking_db.sql
@@ -0,0 +1,35 @@
+-- Tabela de Usuários com prefixo
+CREATE TABLE ranking_usuarios (
+    id VARCHAR(36) PRIMARY KEY,
+    departamento ENUM(
+        'Cadastro',
+        'Informatica',
+        'Gerencia',
+        'Frente de Caixa'
+    ) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    senha VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+-- Tabela de Operadoras com prefixo
+CREATE TABLE ranking_operadoras (
+    numero_operadora INT PRIMARY KEY,
+    nome_operadora VARCHAR(255) NOT NULL
+);
+-- Tabela para o Último Relatório com prefixo
+CREATE TABLE ranking_ultimo_relatorio (
+    id INT PRIMARY KEY DEFAULT 1,
+    dados_relatorio JSON NOT NULL,
+    data_geracao TIMESTAMP NOT NULL
+);
+-- Tabela para o Histórico de Relatórios com prefixo
+CREATE TABLE ranking_relatorios_salvos (
+    id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    usuario_id VARCHAR(36) NOT NULL,
+    data_inicial DATE NOT NULL,
+    data_final DATE NOT NULL,
+    data_geracao TIMESTAMP NOT NULL,
+    dados_relatorio JSON NOT NULL,
+    FOREIGN KEY (usuario_id) REFERENCES ranking_usuarios (id) ON DELETE CASCADE
+);
